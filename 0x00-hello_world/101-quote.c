a61c0afe0316f181c8dee220e802e1f96ca0e4ce
rachel648 <rachelraqelmurithi@gmail.com>
2023-03-16 12:05:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
-/**
- * main - A program that prints a line using the printf function
- * Return: 0 (Success)
+#include<unistd.h>
+
+/*
+ * main - Entry point
+ * Description: print a quote using write function
+ *
+ * ssize_t write(int fd, const void *buf, size_t count);
+ * Return: This time we return an error 1.
  */
+
 int main(void)
 {
-printf("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
-return (0);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
 }
