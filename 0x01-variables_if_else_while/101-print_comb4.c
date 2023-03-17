aafa7b6314a1b5348ac5c059612602d6ed36f35d
rachel648 <rachelraqelmurithi@gmail.com>
2023-03-17 07:48:53 -0700

    g

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,20 @@
-/*
- * File: 101-print_comb4.c
- * Auth: Brennan D Baraban
- */
-
-#include <stdio.h>
-
-/**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int digit1, digit2, digit3;
-
-	for (digit1 = 0; digit1 < 8; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
-
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+int main() {
+    int i, j, k;
+    for (i = 0; i < 8; i++) {
+        for (j = i + 1; j < 9; j++) {
+            for (k = j + 1; k < 10; k++) {
+                int smallest = (i < j) ? ((i < k) ? i : k) : ((j < k) ? j : k);
+                int middle = (i + j + k) - smallest - ((i > j) ? ((i > k) ? i : k) : ((j > k) ? j : k));
+                int largest = (i + j + k) - smallest - middle;
+                putchar(smallest + '0');
+                putchar(middle + '0');
+                putchar(largest + '0');
+                if (i < 7 || j < 8 || k < 9) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return 0;
 }
-
