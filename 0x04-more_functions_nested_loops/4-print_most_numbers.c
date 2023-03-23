08e6b2f698cc064dc1feae48869802907bb80394
rachel648 <rachelraqelmurithi@gmail.com>
2023-03-23 10:15:48 -0700

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,27 @@
-#include "mainn.h"
+#include "main.h"
+
+
 
 /**
- * print_most_numbers -  checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ *print_most_numbers - prints most numbers for the series
+ *Return: void
  */
-void print_most_numbers(void)
-{
-	int a;
 
-	for (a = 48; a < 58; a++)
-	{
-		if (a != 50)
-		{
-			if (a != 52)
-			{
-			_putchar(a);
-			}
-		}
-	}
-	_putchar('\n');
+
+void print_most_numbers(void)
+
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+if (!(n == '2' || n == '4'))				
+_putchar(n);
 }
+_putchar('\n');
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priv
 
