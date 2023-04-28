0bc2da05c9a54b61acd6410906347b18dbe84c02
rachel648 <rachelraqelmurithi@gmail.com>
2023-04-28 01:43:41 -0700

    j

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: the number of nodes printed
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
@@ -18,3 +18,4 @@ size_t list_len(const list_t *h)
 	}
 	return (n);
 }
+
