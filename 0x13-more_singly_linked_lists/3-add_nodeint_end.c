16cc5cd18236c3b0ac0633c95cd24bf168f5e72e
rachel648 <rachelraqelmurithi@gmail.com>
2023-04-29 06:16:53 -0700

    i

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,10 @@
 #include "lists.h"
-
 /**
- * add_nodeint_end - adds a node at the end of a linked list
- * @head: pointer to the first element in the list
- * @n: data to insert in the new element
+ * add_nodeint_end - it adds a node at the end of a linked list
+ * @head: is pointer to the first element in the list.
+ * @n: data to insert in the new element.
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return:the pointer to the new node or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
-#include "lists.h"
 
+#include "lists.h"
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - .. frees a linked list
+ * @head: -pointer to the listint_t list to be freed.
  */
 void free_listint2(listint_t **head)
 {
