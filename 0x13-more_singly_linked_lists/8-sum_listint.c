6edd5a8e7ed498e665f36d5ef7bcebd9ea81bbd2
rachel648 <rachelraqelmurithi@gmail.com>
2023-04-29 06:27:18 -0700

    o

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,12 @@
-#include "lists.h"
 
+#include "lists.h"
 /**
- * sum_listint - calculates the sum of all the data in a listint_t list
- * @head: first node in the linked list
+ * sum_listint - .. calculates the sum of all the data in a listint_t list
+ * @head: -first node in  linked list
  *
  * Return: resulting sum
  */
+
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,12 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
+ * at a given position.
  * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * @idx: ..index where the new node is added
+ * @n: data to insert in the new node.
  *
- * Return: pointer to the new node, or NULL
+ * Return: pointer to the new node or NULL..
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
